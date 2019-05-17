e4554f08755f6c0ac9f91b31eea4ada043eabf90
Maxim Butin <butinmv9722@gmail.com>
2019-05-18 00:03:48 +0700

    Add header file

---
+++ ComPOSiOS/ComPOSiOS/ComPOSiOS-Bridging-Header.m
@@ -0,0 +1,10 @@
+//
+//  ComPOSiOS-Bridging-Header.m
+//  ComPOSiOS
+//
+//  Created by Maxim Butin on 17/05/2019.
+//  Copyright © 2019 Maxim Butin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <fptr10/libfptr10_objc.h>
