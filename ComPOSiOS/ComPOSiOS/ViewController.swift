d10324c1c96aeccaa25fb0812b0b92fdaceb046f
Maxim Butin <butinmv9722@gmail.com>
2019-05-17 23:32:55 +0700

    Add iOS folder

---
+++ ComPOSiOS/ComPOSiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ComPOSiOS
+//
+//  Created by Maxim Butin on 17/05/2019.
+//  Copyright © 2019 Maxim Butin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
