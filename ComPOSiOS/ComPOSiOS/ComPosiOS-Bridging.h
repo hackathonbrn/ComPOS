2acf51b43bc7b94c94f18aa197745d693714f6d8
Maxim Butin <butinmv9722@gmail.com>
2019-05-18 09:56:16 +0700

    print text

---
+++ ComPOSiOS/ComPOSiOS/ComPosiOS-Bridging.h
@@ -0,0 +1,14 @@
+//
+//  ComPosiOS-Bridging.h
+//  ComPOSiOS
+//
+//  Created by Maxim Butin on 18/05/2019.
+//  Copyright © 2019 Maxim Butin. All rights reserved.
+//
+
+#ifndef ComPosiOS_Bridging_h
+#define ComPosiOS_Bridging_h
+
+#import <fptr10/libfptr10_objc.h>
+
+#endif /* ComPosiOS_Bridging_h */
--- ComPOSiOS/ComPOSiOS/ViewController.swift
+++ ComPOSiOS/ComPOSiOS/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let fptr = IFptr()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        fptr?.setSingleSetting(LIBFPTR_SETTING_IPADDRESS, value: "192.168.1.146")
+        fptr?.setSingleSetting(LIBFPTR_SETTING_PORT, value: "2")
+        fptr?.setSingleSetting(LIBFPTR_SETTING_IPPORT, value: "5555")
+        fptr?.applySingleSettings()
+        
+        fptr?.getSettings()
     }
-
-
+    
+    @IBAction func connect(_ sender: Any) {
+        fptr?.open()
+        
+    }
+    
+    @IBAction func print(_ sender: Any) {
+        fptr?.setParam(Int32(LIBFPTR_PARAM_TEXT.rawValue), nsStringParam: "Hackathon Barnaul")
+        fptr?.printText()
+    }
+    
+    @IBAction func close(_ sender: Any) {
+        fptr?.close()
+    }
 }
 
